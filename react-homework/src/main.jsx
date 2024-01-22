3bd24ebeb4bb38e1e2020a55c78afe7b194fbedd
roben31380 <kinalee0430@gmail.com>
2024-01-23 00:00:39 +0900

    Feat: App 생성, logo 담아보기

--- react-homework/src/main.jsx
+++ react-homework/src/main.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+// import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 function Logo() {
   return <img src="./src/assets/react.svg" alt="리액트" />;
 }
 
+function App() {
+  return (
+    <div className="App">
+      <Logo />
+    </div>
+  );
+}
+
 const domElement = document.getElementById('root');
 if (domElement) {
   const reactDomElement = createRoot(domElement);
-  reactDomElement.render(<Logo />);
+  reactDomElement.render(<App />);
 }
