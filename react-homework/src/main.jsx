bfcc75e3158160e023f9c581e9e7abfd915b82b6
roben31380 <kinalee0430@gmail.com>
2024-01-22 23:56:19 +0900

    Feat: logo를 jsx 문법, 함수로 렌더링

--- react-homework/src/main.jsx
+++ react-homework/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+function Logo() {
+  return <img src="./src/assets/react.svg" alt="리액트" />;
+}
+
+const domElement = document.getElementById('root');
+if (domElement) {
+  const reactDomElement = createRoot(domElement);
+  reactDomElement.render(<Logo />);
+}
