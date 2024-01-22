7c20482312cb22d2623774c4e89032767911862d
roben31380 <kinalee0430@gmail.com>
2024-01-23 00:56:10 +0900

    Feat: style 설정

--- react-homework/src/Logo.jsx
+++ react-homework/src/Logo.jsx
@@ -1,5 +1,17 @@
+const logoImage = {
+  src: '/src/assets/react.svg',
+  alt: '리액트',
+};
+
+const figStyle = { margin: 0, display: 'flex', alignItems: 'center', gap: 12 };
+
 function Logo() {
-  return <img src="./src/assets/react.svg" alt="리액트" />;
+  return (
+    <figure style={figStyle}>
+      <img src={logoImage.src} alt="" />
+      <figcaption>{logoImage.alt}</figcaption>
+    </figure>
+  );
 }
 
 export default Logo;
