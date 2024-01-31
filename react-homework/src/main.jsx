b498143551399e4ceca1e8e1e99593385b7b56f9
roben31380 <kinalee0430@gmail.com>
2024-01-31 22:58:34 +0900

    Fix: 폴더가 잘못담겨서 서버가 구동안되는 문제 해결

--- react-homework/src/main.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-
-const domElement = document.getElementById('root');
-if (domElement) {
-  const reactDomElement = createRoot(domElement);
-  reactDomElement.render(<App />);
-}
